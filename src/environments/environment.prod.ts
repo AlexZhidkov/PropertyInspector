1544f3d24afa2fc6fdc4bd17268f8adbf35d1850
Alexander Zhidkov <azhidkov@gmail.com>
2018-11-11 16:21:35 +0800

    add AngularFire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCRoDQLXymX0_i5_2cliKcOwOB3ccSs6yc',
+    authDomain: 'propertyinspector-dev.firebaseapp.com',
+    databaseURL: 'https://propertyinspector-dev.firebaseio.com',
+    projectId: 'propertyinspector-dev',
+    storageBucket: 'propertyinspector-dev.appspot.com',
+    messagingSenderId: '457534121948'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCRoDQLXymX0_i5_2cliKcOwOB3ccSs6yc',
+    authDomain: 'propertyinspector-dev.firebaseapp.com',
+    databaseURL: 'https://propertyinspector-dev.firebaseio.com',
+    projectId: 'propertyinspector-dev',
+    storageBucket: 'propertyinspector-dev.appspot.com',
+    messagingSenderId: '457534121948'
+  }
 };
 
 /*
