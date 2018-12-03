0eb9b9821eac524f9a7e44e5f096638f428786e5
Alexander Zhidkov <azhidkov@gmail.com>
2018-12-03 20:43:53 +0800

    #7 function to get image URL

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
+const request = require('request');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.getImageUrl = functions.https.onRequest((req, res) => {
+    const url = req.query.url;
+    console.log('request url = ' + url);
+    request.get(url, (error, response, body) => {
+        const start = body.indexOf('<meta property="og:image" content="');
+        console.log('start = ' + start);
+        const imageUrlStart = body.substring(start + 35);
+        const end = imageUrlStart.indexOf('=');
+        console.log('end = ' + end);
+        const imageUrl = imageUrlStart.substring(0, end)
+        console.log('imageUrl = ' + imageUrl);
+        res.send({ url: imageUrl });
+    })
+});
